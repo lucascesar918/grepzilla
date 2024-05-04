93d57cfef09d3cecad8279eba48d9b80f9181e8d
lucascesar918 <52894434+lucascesar@users.noreply.github.com>
2024-05-04 16:27:34 -0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Expected more arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let file_path = &args[2];
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Couldn't read file!");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("{contents}");
+    println!("{}", contents);
+
+    Ok(())
 }
