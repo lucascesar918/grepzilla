2a21eb059e27158de234f3a1f7b596e8e02950d8
lucascesar918 <52894434+lucascesar@users.noreply.github.com>
2024-05-05 19:27:03 -0300

    separated logic from main and first grep done

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,15 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
-
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Expected more arguments!");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
+use grepzilla::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Application error: {e}");
+    if let Err(e) = grepzilla::run(config) {
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("{}", contents);
-
-    Ok(())
 }
