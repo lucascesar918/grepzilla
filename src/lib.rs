2c775b9b9115e32afc2ec3b20365cba5c2bcb041
lucascesar918 <52894434+lucascesar@users.noreply.github.com>
2024-05-05 21:13:30 -0300

    add case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,40 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 
     #[test]
     fn one_result() {
@@ -13,22 +44,27 @@ Rust:
 safe, fast, productive.
 Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
     }
 }
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config { query, file_path }
-    }
+//    pub fn new(args: &[String]) -> Config {
+//        let query = args[1].clone();
+//        let file_path = args[2].clone();
+//        let ignore_case = args[3].clone();
+//
+//        Config { query, file_path, ignore_case }
+//    }
 
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
@@ -37,15 +73,25 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = match config.ignore_case {
+        true => search_case_insensitive(&config.query, &contents),
+        false => search(&config.query, &contents),
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -63,3 +109,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     results
 }
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use grepzilla::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|_err| {
         process::exit(1);
     });
 
-    if let Err(e) = grepzilla::run(config) {
+    if let Err(_e) = grepzilla::run(config) {
         process::exit(1);
     }
 }
