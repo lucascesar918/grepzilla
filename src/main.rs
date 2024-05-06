cf91a8cb6b7e632eaaa6303fd1150c47ee1849d6
lucascesar918 <52894434+lucascesar@users.noreply.github.com>
2024-05-05 23:07:09 -0300

    fix error print to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use grepzilla::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Bad usage: {err}");
+        eprintln!("Bad usage: {err}");
         grepzilla::print_help();
         process::exit(1);
     });
 
-    if let Err(_e) = grepzilla::run(config) {
+    if let Err(e) = grepzilla::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
